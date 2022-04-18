4e40ca7c973d4f073fe36a86c6e6a4b8cf3094c0
Haleigh Haus <haleighhaus@Haleighs-MacBook-Pro-2.local>
2022-04-18 17:40:37 +0100

    Add coding challenge for 04-18-2022

---
+++ CC 2022-04-18/app.js
@@ -0,0 +1,13 @@
+//Write a function, agreeOrDisagree(), that takes in two strings, and returns 'You agree!' if the two strings are the same and 'You disagree!' if the two strings are different.
+
+// Write your function here:
+const agreeOrDisagree = (str1,str2) => str1 === str2 ?
+'You agree!' : 'You disagree!'
+
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(agreeOrDisagree("yep", "yep")) 
+// Should print 'You agree!'
